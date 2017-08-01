35ee6cde64d153e1c4c881fbb244bbc35290f8b0
Vyom Maitreya <vyommaitreya@gmail.com>
2017-08-01 23:34:33 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    session_start();
+?>
+    <html>
+
+    <head>
+        <title>Register - UPES ACM/ACM-W Student Chapters</title>
+
+        <link href="assets/css/index.css" rel="stylesheet" />
+    </head>
+
+    <body>
+        <header>
+            <hgroup>
+                <h1><a href="." class="brand">UPES ACM/ACM-W Student Chapters</a></h1>
+            </hgroup>
+        </header>
+    </body>
+
+    </html>
