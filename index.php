9765bbbe0391eaa724210f33ae5a471a0a7203d4
wraith-of-peril <vyommaitreya@gmail.com>
2017-08-02 04:16:05 +0530

    fontend

--- index.php
+++ index.php
@@ -12,9 +12,35 @@
     <body>
         <header>
             <hgroup>
-                <h1><a href="." class="brand">UPES ACM/ACM-W Student Chapters</a></h1>
+                <h1><a href="." class="brand">register</a></h1>
             </hgroup>
         </header>
+        <section>
+            <article>
+                <center>
+                    <form action="assets/functions/register.php" method="post" id="form">
+                        <input type="text" name="code" id="code" class="inp-txt" placeholder="Unique Code" required />
+                        <div class="left">
+                            <input type="text" name="name" class="inp-txt" placeholder="Name" required />
+                            <input type="text" name="name" class="inp-txt" placeholder="Branch" required />
+                            <input type="email" name="name" class="inp-txt" placeholder="Email ID" required />
+                        </div>
+                        <div class="right">
+                            <input type="number" name="name" class="inp-txt" placeholder="Contact Number" required />
+                            <input type="number" name="name" class="inp-txt" placeholder="WhatsApp Number" />
+                            <select class="inp-txt" required>
+                                <option value="">Membership Duration</option>
+                                <option>1 year membership</option>
+                                <option>4 years membership</option>
+                            </select>
+                        </div>
+                        <div class="button">
+                            <input type="submit" name="submit" value="Register" />
+                        </div>
+                    </form>
+                </center>
+            </article>
+        </section>
     </body>
 
     </html>
