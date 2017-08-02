b738f40fd131bdab2300af2b4aacc322e442fcb7
wraith-of-peril <vyommaitreya@gmail.com>
2017-08-02 19:10:17 +0530

    Added banner

--- index.php
+++ index.php
@@ -7,6 +7,20 @@
         <title>Register - UPES ACM/ACM-W Student Chapters</title>
 
         <link href="assets/css/index.css" rel="stylesheet" />
+
+        <script type="text/javascript">
+            function showImages()
+            {
+                var i=document.getElementsByTagName('img');
+                i[0].style.opacity = 1;
+                i[1].style.opacity = 1;
+            }
+            function hideImages() {
+                var i=document.getElementsByTagName('img');
+                i[0].style.opacity = 0;
+                i[1].style.opacity = 0;
+            }
+        </script>
     </head>
 
     <body>
@@ -17,9 +31,19 @@
         </header>
         <section>
             <article>
+                <div id="gif">
+                    <div id="logos" onmouseover="showImages();" onmouseout="hideImages();">
+                        <center>
+                            <a href="http://upesacm.org" target="_blank"><img src="assets/images/UPES%20ACM%20LOGO.png" /></a>
+                            <a href="http://upesacmwomen.org" target="_blank"><img src="assets/images/ACM-W.png" /></a>
+                        </center>
+                    </div>
+                </div>
+            </article>
+            <article>
                 <center>
                     <form action="assets/functions/register.php" method="post" id="form">
-                        <input type="text" name="code" id="code" class="inp-txt" placeholder="Unique Code" required />
+                        <input type="text" name="code" id="code" class="inp-txt" placeholder="Enter Unique Code Here" required />
                         <div class="left">
                             <input type="text" name="name" class="inp-txt" placeholder="Name" required />
                             <input type="text" name="name" class="inp-txt" placeholder="Branch" required />
